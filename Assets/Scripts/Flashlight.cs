07b38533ff66d495cc889a42767590d658e5fdf9
LEO-PC\Utilisateur <nonard76@gmail.com>
2016-11-18 00:17:44 +0100

    ùù

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+        private const byte INTENSITY_ON = 5;
+    private const byte INTENSITY_OFF = 0;
+		private bool on = false;
+        public GameObject text; 
+		void Start ()
+		{
+				gameObject.GetComponent<Light>().intensity = INTENSITY_OFF;
+		}
+
+		void Update ()
+		{
+			if (Input.GetKeyDown(KeyCode.F)) {
+            on = !on;
+            text.SetActive(false); // Pour le début
+        }
+
+		}
+
+    void OnGUI()
+    {
+        if (!on)
+        {
+            gameObject.GetComponent<Light>().intensity = INTENSITY_ON;
+            on = !on;
+        }
+        else
+        {
+            gameObject.GetComponent<Light>().intensity = INTENSITY_OFF;
+            on = !on;
+        }
+    }
+}
